6dda61d4aa2efcd6eae98fd627d5c15f3296a62f
Praveen Koru <praveenkoru@Praveens-MacBook-Pro.local>
2021-03-01 09:06:32 -0700

    Updates

---
+++ precache-manifest.321750af89b071549d60101d44afd417.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53b759d1c8881fc1d7ad1999a36bbff",
+    "url": "/React-Visualization-Chart/index.html"
+  },
+  {
+    "revision": "902b58603cd1370a60bf",
+    "url": "/React-Visualization-Chart/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "902b58603cd1370a60bf",
+    "url": "/React-Visualization-Chart/static/js/2.4091d4cf.chunk.js"
+  },
+  {
+    "revision": "2eef8b706b4f8e9ee0c1",
+    "url": "/React-Visualization-Chart/static/js/main.3f0cd063.chunk.js"
+  },
+  {
+    "revision": "3abc5fcb8c3ddfd93904",
+    "url": "/React-Visualization-Chart/static/js/runtime-main.d3a152c4.js"
+  }
+]);
